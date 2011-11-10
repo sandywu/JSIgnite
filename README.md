2dad0b1b5e2ef478a27d1b5b22cab6ec823018d8
Timothy Warren <tim@timshomepage.net>
2011-11-10 14:08:52 -0500

    Fixed formatting in server.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 JSIgnite is a basic starting point for a NodeJS application. 
+
+For now, this is just a skeleton app, with a basic server as a starting point. 
--- core/server.js
+++ core/server.js
@@ -3,20 +3,21 @@ var http = require("http"),
 	path = require("path");
 
 function start(route, handle) {
-  function onRequest(request, response) {
-  	
-  	//Parse the pathname of the url
-  	var pathname = url.parse(request.url).pathname;
-  	
-  	//Log the request
-    console.log("Request for " + pathname + " received.");
-    
-    //Send the request to the router to determine where to go from here
-	route(handle, pathname, response);
-  }
-
-  http.createServer(onRequest).listen(8124);
-  console.log("Server has started.");
+	
+	function onRequest(request, response) {
+	
+		//Parse the pathname of the url
+		var pathname = url.parse(request.url).pathname;
+		
+		//Log the request
+		console.log("Request for " + pathname + " received.");
+		
+		//Send the request to the router to determine where to go from here
+		route(handle, pathname, response);
+	}
+	
+	http.createServer(onRequest).listen(8124);
+	console.log("Server has started.");
 }
 
 exports.start = start;
